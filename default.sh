08f67dfa6283918797679d13d7f39df1942fd7d7
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2021-12-06 01:16:08 -0800

    Update default.sh

--- default.sh
+++ default.sh
@@ -16,7 +16,7 @@ sudo apt-get install git -y
 echo ""
 echo ""
 echo "## [UPDATE SCRIPT MESSAGE]:	Node 16.x installing..."
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
