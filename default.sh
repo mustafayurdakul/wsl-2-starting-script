9b87c7e765c9c267435e7cc85aea13a1be322f88
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2022-04-12 11:27:21 +0300

    Update default.sh

--- default.sh
+++ default.sh
@@ -15,7 +15,7 @@ sudo apt-get install git -y
 
 echo ""
 echo ""
-echo "## [UPDATE SCRIPT MESSAGE]:	Node 16.x installing..."
+echo "## [UPDATE SCRIPT MESSAGE]:	Node Lastest installing..."
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
