724c91fd0d55fd93a7787fd2bbb746af109579e6
Mustafa Yurdakul <mustafa@yurdakul.dev>
2023-02-22 18:31:15 +0300

    Update default.sh

--- default.sh
+++ default.sh
@@ -16,7 +16,7 @@ sudo apt-get install git -y
 echo ""
 echo ""
 echo "## [UPDATE SCRIPT MESSAGE]:	Node Lastest installing..."
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
